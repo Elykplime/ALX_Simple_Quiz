86743dddd141aea2a01f0f3f454e3dabbded6663
Elykplime <akakpoyvettee@gmail.com>
2024-07-11 19:22:26 +0000

    changes made to js file

--- quiz.js
+++ quiz.js
@@ -8,7 +8,7 @@ function checkAnswer() {
         document.getElementById ("feedback").textContent = "Correct! Well done.";
     }
     else {
-        document.getElementById ("feedback").textContent = "That's incorrect. Try again";
+        document.getElementById ("feedback").textContent = "That's incorrect. Try again!";
     }
 }
 
