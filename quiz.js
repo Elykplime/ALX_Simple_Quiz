ad9724bcfe1d463f010b26e8fc751ff4dba6679b
Elykplime <akakpoyvettee@gmail.com>
2024-07-11 19:16:11 +0000

    javascript file added

---
+++ quiz.js
@@ -0,0 +1,16 @@
+// function declarstion to check answer
+function checkAnswer() {
+    let correctAnswer = "4";
+    // to retrieve user's answer
+    let userAnswer = document.querySelector("input[name= quiz]:checked").value;
+    // to check if the answer is correct
+    if (userAnswer == correctAnswer) {
+        document.getElementById ("feedback").textContent = "Correct! Well done.";
+    }
+    else {
+        document.getElementById ("feedback").textContent = "That's incorrect. Try again";
+    }
+}
+
+// adding event listener to the submit button
+document.getElementById("submit-answer").addEventListener('click', checkAnswer)
